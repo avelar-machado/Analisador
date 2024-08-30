822d5d97b42229aa0be6711ced3631bf86579a54
catiaadaodev <catiaadaofrancisco@gmail.com>
2024-08-30 10:46:09 +0100

    Analisador de Hierarquia de Palavras App

---
+++ analisador-hierarquia-palavras/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Analisador de Hierarquia de Palavras</h1>
+    </div>
+  );
+}
+
+export default App;
